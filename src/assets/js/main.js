f13253beca58ab81406c154fcabefb8e429520b3
kryukova-ana <morinoshika13@gmail.com>
2021-11-29 15:45:34 +0300

    v 1.6

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,8 +1,12 @@
-function myFunction() {
-    var x = document.getElementById("myInput");
+function showPass() {
+    var y = document.getElementById("newImg");
+
+    var x = document.getElementById("user-password");
     if (x.type === "password") {
         x.type = "text";
+        y.src ='../../assets/img/open-eye.svg';
     } else {
         x.type = "password";
+        y.src= "../../assets/img/eye-off.svg";
     }
 }
